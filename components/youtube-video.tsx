3d8815e093a8980b2821a00d62dbc34bc8aa6559
Meek Msaki <meek10x@gmail.com>
2025-06-02 04:34:38 -0500

    make video aspect-ratio

--- components/youtube-video.tsx
+++ components/youtube-video.tsx
@@ -4,8 +4,8 @@ type VideoProps = {
 export default function YouTubeVideo(props: VideoProps) {
 	const url = `https://www.youtube.com/embed/${props.videoId}`
 	return (
-		<div className="video-container">
-			<iframe width="560" height="315" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+		<div className="aspect-video">
+			<iframe className="w-full h-full" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 		</div >
 	);
 }
